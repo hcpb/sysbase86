85a228905ca2bee047af14d4439ab270586c6885
dgd <icenyne@gmail.com>
2016-07-08 11:28:37 -0500

    updated script...

--- install.sh
+++ install.sh
@@ -4,6 +4,14 @@ apt-get -y install apache2 samba netatalk python-serial python-pygame \
 useradd -G dialout,tty dgd
 
 wget http://webmin.com/download/deb/webmin-current.deb
+dpkg -i webmin-current.deb
+rm webmin-current.deb
 
-dpkg -i *.deb
+cd /home/dgd
 
+git clone http://github.com/alexdu/piggyphoto
+cp -r piggyphoto/piggyphoto /usr/lib/python2.7
+rm -rf piggyphoto
+
+git clone http://github.com/hcpb/photobooth
+chown -R dgd:dgd photobooth 
