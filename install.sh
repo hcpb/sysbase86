02f939c0ed6a4915c5bbe84f4597d9c3f063cfc2
dgd <icenyne@gmail.com>
2016-07-09 13:19:38 -0500

    fixed typo...

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get -y install apache2 samba netatalk python-serial python-pygame \
 	graphicsmagick libgphoto2-dev gphoto2 git libauthen-pam-perl \
-	apt-show-versions dconf-editor gconf-editor openssh-server <\
+	apt-show-versions dconf-editor gconf-editor openssh-server \
 	gparted
 
 wget http://webmin.com/download/deb/webmin-current.deb
